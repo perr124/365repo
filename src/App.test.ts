0b809866e3b46f75435c2e3d965c494ef3b640a7
projectvay <projectvay@gmail.com>
2021-08-14 17:00:19 -0500

    update test file

--- src/App.test.ts
+++ src/App.test.ts
@@ -3,18 +3,6 @@ import { Selector } from 'testcafe';
 fixture`Getting Started`
     .page`https://widgets-475f6.web.app/`;
 
-
-test('Submit form with all fields filled', async t => {
-    const evalName = 'Evalname';
-
-    const evalNameInput = Selector('#evalName');
-    const evalNameInputExists = evalNameInput.exists;
-    await t
-        .expect(evalNameInputExists).ok()
-        .typeText(evalNameInput, evalName)
-        .expect(evalNameInput.value).eql(evalName)
-});
-
 test("Check uploaded files", async (t) => {
     await t
         .setFilesToUpload("#file-input", ["../../readings.txt"])
